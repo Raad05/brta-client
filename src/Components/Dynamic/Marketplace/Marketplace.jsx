564d7b55a4956b1be92510257266af20a4a275f9
Raad05 <ashabulraad@gmail.com>
2024-07-04 19:15:39 +0600

    navbar completed

--- src/Components/Dynamic/Marketplace/Marketplace.jsx
+++ src/Components/Dynamic/Marketplace/Marketplace.jsx
@@ -1,21 +1,32 @@
 import logo from "../../../assets/drivechain.png";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Marketplace = () => {
   return (
-    <div className="marketplace container mx-auto">
-      <nav className="flex justify-between">
-        <div className="w-1/2 flex justify-between items-center">
+    <div className="marketplace px-5 text-orange-500">
+      <nav className="flex">
+        <div className="w-1/2 flex justify-around items-center">
           <div className="flex items-center text-xl font-bold">
             <img src={logo} alt="drivechain" /> DriveChain
           </div>
           <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
+            <input
+              type="text"
+              className="grow"
+              placeholder="search for collectibles"
+            />
             <kbd className="kbd kbd-sm">⌘</kbd>
             <kbd className="kbd kbd-sm">K</kbd>
           </label>
-          <div>Nav links</div>
+          <div>
+            <button className="mx-2 text-lg font-bold">Explore</button>
+            <button className="mx-2 text-lg font-bold">My NFTs</button>
+          </div>
         </div>
-        <div className="w-1/2 flex justify-end">user info</div>
+        <div className="w-1/2 flex justify-end items-center mr-10">
+          <button className="mx-5 text-lg font-bold">Login</button>
+          <FaRegUserCircle size={30} className="text-black"></FaRegUserCircle>
+        </div>
       </nav>
     </div>
   );
