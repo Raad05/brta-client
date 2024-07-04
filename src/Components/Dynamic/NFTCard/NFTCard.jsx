a2e110e8f5194b4427c684a01a0e730e7c07e0df
Raad05 <ashabulraad@gmail.com>
2024-07-04 19:29:13 +0600

    NFT card

---
+++ src/Components/Dynamic/NFTCard/NFTCard.jsx
@@ -0,0 +1,25 @@
+import iln from "../../../assets/ILN.png";
+
+const NFTCard = () => {
+  return (
+    <div className="nft-card max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white">
+      <img className="w-full" src={iln} alt="Summer is ON" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">
+          ILN NFT<span className="text-yellow-500">🌟</span>
+        </div>
+
+        <p className="text-base">Very Dhanda</p>
+      </div>
+      <div className="p-4 mx-4 mb-4 rounded-lg flex justify-between items-center bg-gray-700">
+        <div className="text-green-500 flex items-center">
+          <span className="dot bg-green-500 rounded-full h-2 w-2 mr-2"></span>{" "}
+          Owned
+        </div>
+        <p className="font-bold">0.01 ETH</p>
+      </div>
+    </div>
+  );
+};
+
+export default NFTCard;
