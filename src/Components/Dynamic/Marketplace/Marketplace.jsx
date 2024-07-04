88c091762f97d094f0c28b9e04b8575f62a6872a
Raad05 <ashabulraad@gmail.com>
2024-07-04 16:05:50 +0600

    search bar

--- src/Components/Dynamic/Marketplace/Marketplace.jsx
+++ src/Components/Dynamic/Marketplace/Marketplace.jsx
@@ -8,7 +8,11 @@ const Marketplace = () => {
           <div className="flex items-center text-xl font-bold">
             <img src={logo} alt="drivechain" /> DriveChain
           </div>
-
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <kbd className="kbd kbd-sm">⌘</kbd>
+            <kbd className="kbd kbd-sm">K</kbd>
+          </label>
           <div>Nav links</div>
         </div>
         <div className="w-1/2 flex justify-end">user info</div>
