0c28e56ff770596e91830d9894e6e13fca280e17
Raad05 <ashabulraad@gmail.com>
2024-07-16 18:52:10 +0600

    new images added

--- src/Components/Dynamic/Marketplace/Marketplace.jsx
+++ src/Components/Dynamic/Marketplace/Marketplace.jsx
@@ -2,6 +2,79 @@ import logo from "../../../assets/drivechain_new.png";
 import { FaRegUserCircle } from "react-icons/fa";
 import NFTCard from "../NFTCard/NFTCard";
 import { Link } from "react-router-dom";
+import skyline from "../../../assets/cars/skyline.jpg";
+import supra from "../../../assets/cars/supra.jpg";
+import mazda from "../../../assets/cars/mazda.jpg";
+import audi from "../../../assets/cars/audi.jpg";
+import lambo from "../../../assets/cars/lamborghini.jpg";
+import merce from "../../../assets/cars/audi.jpg";
+import challenger from "../../../assets/cars/challenger.jpg";
+import mustang from "../../../assets/cars/mustang.jpg";
+import r35 from "../../../assets/cars/r35.jpg";
+import corolla from "../../../assets/cars/corolla.jpg";
+
+const cars = [
+  {
+    image: skyline,
+    name: "Skyline R34",
+    description: "Nissan Skyline R34 V-spec II",
+    price: "0.1",
+  },
+  {
+    image: supra,
+    name: "Supra",
+    description: "Toyota Supra Mark 4",
+    price: "0.1",
+  },
+  {
+    image: r35,
+    name: "Nissan",
+    description: "Nissan GTR 35",
+    price: "0.1",
+  },
+  {
+    image: corolla,
+    name: "Toyota",
+    description: "Toyota Corolla 1997",
+    price: "0.1",
+  },
+  {
+    image: mazda,
+    name: "Mazda",
+    description: "Mazda RX 7",
+    price: "0.1",
+  },
+  {
+    image: audi,
+    name: "Audi",
+    description: "Audi R8",
+    price: "0.1",
+  },
+  {
+    image: lambo,
+    name: "Lamborghini",
+    description: "Lamborghini Centenario",
+    price: "0.1",
+  },
+  {
+    image: merce,
+    name: "Mercedes",
+    description: "Mercedes Benz",
+    price: "0.1",
+  },
+  {
+    image: challenger,
+    name: "Dodge",
+    description: "Dodge Charger Hellcat",
+    price: "0.1",
+  },
+  {
+    image: mustang,
+    name: "Ford",
+    description: "Ford Mustang Shelby GT500",
+    price: "0.1",
+  },
+];
 
 const Marketplace = () => {
   return (
@@ -29,14 +102,9 @@ const Marketplace = () => {
       <div>
         <h3 className="text-2xl mx-10 mt-5 mb-10 font-bold">Owned NFTs</h3>
         <div className="grid grid-cols-5 gap-y-5 mx-10">
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
-          <NFTCard></NFTCard>
+          {cars.map((car, idx) => (
+            <NFTCard key={idx} info={car}></NFTCard>
+          ))}
         </div>
       </div>
     </div>
--- src/Components/Dynamic/NFTCard/NFTCard.jsx
+++ src/Components/Dynamic/NFTCard/NFTCard.jsx
@@ -1,22 +1,23 @@
-import iln from "../../../assets/avatar7 2.png";
+/* eslint-disable react/prop-types */
+const NFTCard = ({ info }) => {
+  const { image, name, description, price } = info;
 
-const NFTCard = () => {
   return (
     <div className="nft-card max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white w-80">
-      <img className="w-full" src={iln} alt="Summer is ON" />
+      <img className="w-full" src={image} alt="Summer is ON" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">
-          Avatar NFT<span className="text-yellow-500">🌟</span>
+          {name} NFT<span className="text-yellow-500">🌟</span>
         </div>
 
-        <p className="text-base">Demo Avatar NFT Description</p>
+        <p className="text-base">{description}</p>
       </div>
       <div className="p-4 mx-4 mb-4 rounded-lg flex justify-between items-center bg-gray-700">
         <div className="text-green-500 flex items-center">
           <span className="dot bg-green-500 rounded-full h-2 w-2 mr-2"></span>{" "}
           Owned
         </div>
-        <p className="font-bold">0.01 ETH</p>
+        <p className="font-bold">{price} ETH</p>
       </div>
     </div>
   );
